c01aaab5019d6803393f250fb3366f533b59beae
Mickaël Maury <hello@votre-consultant.digital>
2022-03-25 13:32:32 +0100

    Create minimal_functions_twentytwentyone.php

---
+++ minimal_functions_twentytwentyone.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once(get_stylesheet_directory() . '/inc/post-types.php');
+
+/* Chargement des styles du parent / enfant. */
+add_action( 'wp_enqueue_scripts', 'wpchild_enqueue_styles');
+
+function wpchild_enqueue_styles(){
+  // Enfant (utilisation d'une dépendance de style : twenty-twenty-one-style)
+  wp_enqueue_style( 'twentytwentyone-style-child', get_stylesheet_directory_uri() . '/style.css', 
+    array('twenty-twenty-one-style'));
+}
+
+/* Chargement des fichiers de langue. */
+function twentytwentyone_child_setup() {
+  $child_path = get_stylesheet_directory().'/languages';
+  
+  load_child_theme_textdomain( 'twentytwentyone-child', $child_path );
+}
+
+add_action( 'after_setup_theme', 'twentytwentyone_child_setup' );
+
+function add_div_title( $title, $id = null ) {
+  if(!is_admin()){
+      if(get_post_type($id) === 'film')
+      $title = '<span class="my-custom-title-class">' . $title . '</span>';
+  }
+
+  return $title;
+}
+
+add_filter( 'the_title', 'add_div_title', 10, 2 );
