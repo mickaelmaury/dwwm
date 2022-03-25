453b80b178a97df58cd9d7f1e1ebd1357b0d71bb
Mickaël Maury <hello@votre-consultant.digital>
2022-03-25 10:58:28 +0100

    Create minimal_functions.php

---
+++ minimal_functions.php
@@ -0,0 +1,10 @@
+<?php
+
+/* Chargement des styles du parent / enfant. */
+add_action( 'wp_enqueue_scripts', 'wpchild_enqueue_styles');
+
+function wpchild_enqueue_styles(){
+  // Enfant (utilisation d'une dépendance de style : twenty-twenty-one-style)
+  wp_enqueue_style( 'twentytwentyone-style-child', get_stylesheet_directory_uri() . '/style.css', 
+    array('twenty-twenty-one-style'));
+}
