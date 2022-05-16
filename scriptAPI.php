30e9c6ccbb33f42e0f6124e0d24c1b5fc61c9060
Mickaël Maury <hello@votre-consultant.digital>
2022-05-16 13:03:53 +0200

    Create scriptAPI.php

---
+++ scriptAPI.php
@@ -0,0 +1,23 @@
+<?php
+
+function get($resource, array $params = array()){
+    $apiUrl = 'http://cci.votre-webmaster-freelance.fr/wp-json';
+
+    $json = file_get_contents($apiUrl.$resource.'?'.http_build_query($params));
+
+    $result = json_decode($json);
+
+    return $result;
+}
+
+$pages = get('/wp/v2/pages', array(
+    'orderby' => 'id',
+    'order' => 'asc',
+    //'search' => '',
+));
+
+foreach ($pages as $page) {
+    echo 'Page ' . $page->id . ' : ' . $page->slug . '<br>';
+    echo substr($page->excerpt->rendered, 0, 100) . '...';
+    echo '<hr>';
+}
